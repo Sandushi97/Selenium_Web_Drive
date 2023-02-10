782ea750838520d39cb194a666e9a6d4bd50ad85
Sandushi Rathnayaka <108363078+Sandushi97@users.noreply.github.com>
2023-02-10 15:10:52 +0530

    Update EbayProducts.java

--- Selenium Codes/EbayProducts.java
+++ Selenium Codes/EbayProducts.java
@@ -1,3 +1,5 @@
+package Ebay_Products_New;
+
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -6,112 +8,85 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class EbayProducts {
+public class Ebay {
+	
 	WebDriver driver=new ChromeDriver();
-
+	String url="https://www.ebay.com/";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		Ebay ebay=new Ebay();
+		ebay.openBrowser();
+		ebay.enterProduct("Apple Watches", "All Categories");
+		ebay.search();
+		ebay.printResultOfProduct();
+		ebay.printnthproduct(3);
+		ebay.printallfromfirstpage();
+		ebay.scroll();
 		
-		EbayProducts ebay=new EbayProducts();
-		ebay.OpenBrowser();
-		
-		
-		ebay.EnterProduct("Apple Watch","cell phones & Accessories");
-		ebay.Search();
-		ebay.Print();
-		ebay.PrintNthResult(5);
-		ebay.PrintAllProducts();
-		ebay.printAllResultWithScroll();
-
 	}
-	//*[@id="srp-river-results"]/ul/li[6]
-	//(1)A method to open the Browser
-	public void OpenBrowser() {
-		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
-		driver.get("https://www.ebay.com/");
+	public void openBrowser() {
+		driver.get(url);
 		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
 	}
-	/*public void EnterProduct() {
-		driver.findElement(By.xpath("//*[@id=\"gh-ac\"]")).sendKeys("Apple Watches");
-	}*/
-	//(2)A method to enter a product in search bar
-	public void EnterProduct(String Product,String Category) {
-		driver.findElement(By.xpath("//*[@id=\"gh-ac\"]")).sendKeys(Product);
-		driver.findElement(By.xpath("//*[@id=\"gh-cat\"]")).sendKeys(Category);
+	public void enterProduct(String product,String Category) {
+		WebElement pro=driver.findElement(By.xpath("//*[@id=\"gh-ac\"]"));
+		pro.sendKeys(product);
+		
+		WebElement cate=driver.findElement(By.xpath("//*[@id=\"gh-cat\"]"));
+		cate.sendKeys(Category);
 		
 		
 		
 	}
-	//(3)A method to search 
-	public void Search() {
-		driver.findElement(By.xpath("//*[@id=\"gh-btn\"]")).click();
+	public void search() {
+		WebElement save=driver.findElement(By.xpath("//*[@id=\"gh-btn\"]"));
+		save.click();
 	}
-	//(4)A method to print all Results
-	public void Print() {
-		String results = driver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[1]/div/div[2]/div[1]/div[1]/h1")).getAttribute("textContent");
-		System.out.println(results);
+	public void printResultOfProduct() {
+		WebElement result=driver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[1]/div/div[2]/div[1]/div[1]/h1"));
+		String kk=result.getText();
+		System.out.println(kk);
+		System.out.println("########################################################################");
 		
 	}
-	//(5)A method to  print Nth product details
-	public void PrintNthResult(int number) {
+	public void printnthproduct(int number) {
 		number=number+1;
-		WebElement product=driver.findElement(By.xpath("//*[@id=\"srp-river-results\"]/ul/li["+number+"]")); 
-		String aa=product.getText();
-		System.out.println("##############################################################################################################");
-		System.out.println(aa);
-		System.out.println("##############################################################################################################");
-		
+		WebElement element=driver.findElement(By.xpath("//*[@id=\"srp-river-results\"]/ul/li["+number+"]"));
+		String nthproduct=element.getText();
+		System.out.println(nthproduct);
+		System.out.println("########################################################################");
+	}
+	public void printallfromfirstpage() {
+		//WebElement element=driver.findElement(By.xpath("//*[@id=\"srp-river-results\"]/ul/li"));
+		///String allp=element.getText();
+		//System.out.println(allp);
+		List <WebElement> li=driver.findElements(By.xpath("//*[@id=\"srp-river-results\"]/ul/li"));
+		for(WebElement x:li) {
+			System.out.println(x.getText());
+			System.out.println("*********************************************************");
+			
+		}
 	}
 	
-
-	/*public void printNthResult(int num) {
-		List<WebElement> allProducts = driver.findElements(By.xpath("//*[@id=\"srp-river-results\"]/ul/li"));
-		int count = 0;
-		for( WebElement product : allProducts){
-			
-			if(count == num) {
-				System.out.println("*********************************************************************");
-				
-				System.out.println(product.getText());
-
-				System.out.println("*********************************************************************");
-			}
-			count++;
-			
-			
-			}
-	}*/
-	
-	//(6)A method to print All Products from 1st page
-	public void PrintAllProducts() {
-		WebElement allProductElement=driver.findElement(By.xpath("//*[@id=\"srp-river-results\"]/ul/li"));
-		String bb=allProductElement.getText();
-		System.out.println(bb);
-		System.out.println("###############################################################################################");
-	}
-	//(7) a method to print all products along with scroll down
-	public void printAllResultWithScroll() {
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		
-		List<WebElement> allProducts = driver.findElements(By.xpath("//*[@id=\"srp-river-results\"]/ul/li"));
-		int count = 1;
-		
-		for( WebElement product : allProducts){
-			
-			
-			
-			js.executeScript("arguments[0].scrollIntoView(true);",driver.findElement(By.xpath("//*[@id=\"srp-river-results\"]/ul/li["+count+"]")));
-			
-			System.out.println("*********************************************************************");
-			
-			System.out.println(product.getText());
-
-			System.out.println("*********************************************************************");
-			
+	public void scroll() {
+		JavascriptExecutor js=(JavascriptExecutor)driver;
+		//this is working 
+		//WebElement uptoscroll=driver.findElement(By.xpath("//*[@id=\"srp-river-results\"]/ul/li[61]"));
+		//js.executeScript("arguments[0].scrollIntoView(true)", uptoscroll);
+		List <WebElement> li=driver.findElements(By.xpath("//*[@id=\"srp-river-results\"]/ul/li"));
+		int count=1;
+		for(WebElement x:li) {
+			//System.out.println(x.getText());
+			//System.out.println("*********************************************************");
+			WebElement uptoscroll=driver.findElement(By.xpath("//*[@id=\"srp-river-results\"]/ul/li["+count+"]"));
+			js.executeScript("arguments[0].scrollIntoView(true)", uptoscroll);
+			//System.out.println("########################################################################");
+			//System.out.println(x.getText());
+			//System.out.println("*********************************************************");
 			count++;
 		}
-   
 	}
+	
 
 }
